ae682478f4c2ca6eef1cb3dc48f8e42b4d4aa68b
mariana228228 <maryanabatig123@gmail.com>
2022-03-02 13:27:28 +0200

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const Profile = () => {
+  return (
+
+
+    <div className='content'>
+      <div>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgvC8d9K3K3cE5KFEByBPcazIbyxvt-NA7Iw&usqp=CAU'></img>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
